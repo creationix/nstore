187800cc37fe12771a0f0d6c34c30dfe2fe4c3a6
Tim Caswell <tim@creationix.com>
2010-06-28 12:16:04 -0700

    Got 105,000 inserts/second, now we're talking.

--- test.js
+++ test.js
@@ -2,34 +2,60 @@ var sys = require('sys'),
     Collection = require('./lib/agregar'),
     Step = require('step');
 
-sys.puts("Loading database...");
+checkRam("Startup");
 var users = new Collection("data/users.db");
-sys.puts("Done loading");
+checkRam("After loading db with " + users.length + " documents");
 
-Step(
-    function () {
-        var num = 50000;
-        sys.puts("Inserting " + num + " documents");
-        var group = this.group();
-        for (var i = 0; i < num; i++) {
-            users.save(null, {name: "Tim"+i,age:28}, group());
+function checkRam(message) {
+    sys.puts(message + ":\n" + sys.inspect(process.memoryUsage()));
+}
+
+var num = 1000000;
+var left = num;
+function insert() {
+    if (left > 0) {
+        var counter = 1000;
+        left -= counter;
+        for (var i = 0; i < counter; i++) {
+            users.save(1, {hello: "world"}, function () {
+                counter--;
+                if (counter === 0) {
+                    insert();
+                }
+            });
         }
-    },
-    function (err, results) {
-        sys.puts("Done Inserting");
-        sys.puts("Retrieving all documents...");
-        var data = [];
-        var all = users.stream(function (doc) {
-            return parseInt(doc.key, 10) % 2 === 0;
-        });
-        all.addListener('data', function (doc) {
-            data.push(doc);
-        });
-
-        all.addListener('end', function () {
-            sys.puts("DONE! Loaded " + data.length + " documents.");
-        });
-        
+    } else {
+        checkRam("After queueing " + num + " inserts");
     }
-);
+}
+insert();
 
+process.addListener("exit", function () {
+    checkRam("On Exit");
+})
+
+// Step(
+//     function () {
+//         var num = 10;
+//         sys.puts("Inserting " + num + " documents");
+//         var group = this.group();
+//         for (var i = 0; i < num; i++) {
+//             users.save(null, {name: "Tim"+i,age:28}, group());
+//         }
+//     },
+//     function (err, results) {
+//         sys.puts("Done Inserting");
+//         sys.puts("Retrieving all documents...");
+//         var data = [];
+//         var all = users.stream();
+//         all.addListener('data', function (doc) {
+//             data.push(doc);
+//         });
+// 
+//         all.addListener('end', function () {
+//             sys.puts("DONE! Loaded " + data.length + " documents.");
+//         });
+//         
+//     }
+// );
+// 
