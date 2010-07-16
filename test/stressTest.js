b0e3d510c5f93522b7e8f6e174259d1a938ca9d7
Tim Caswell <tim@creationix.com>
2010-07-16 01:35:33 -0700

    Make nStore much faster and more robust.
    The only tradeoff currently is that the database
    locks for compaction.  This is usually pretty fast
    and doesn't block the event loop.

--- test/stressTest.js
+++ test/stressTest.js
@@ -5,9 +5,9 @@ console.log(sys.inspect(process.memoryUsage()));
 var documents = nStore('fixtures/new.db');
 var counter = 0;
 
-const NUM = 100000;
+const NUM = 1000000;
 for (var i = 0; i < NUM; i++) {
-  documents.save(i, {i:i}, function () {
+  documents.save((i % 1000) + 1, {i:i}, function () {
     counter++;
     if (counter === NUM) {
       documents.compact();
