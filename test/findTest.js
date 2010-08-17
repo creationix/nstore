bffbb486a415cd2bc659a48635a84ba6c808c609
Tim Caswell <tim@creationix.com>
2010-08-16 17:53:47 -0700

    Integrate a simple query language.

---
+++ test/findTest.js
@@ -0,0 +1,67 @@
+require('./helper');
+
+var queries = [
+  [{age: 50}, 2],
+  [{"age =": 50}, 2],
+  [{"age <": 50}, 100],
+  [{"age <=": 50}, 102],
+  [{"age >=": 50}, 100],
+  [{"age >": 50}, 98],
+  [{"age !=": 50}, 198],
+  [{"age <>": 50}, 198],
+  [{age: 50, name: "USER 50"}, 1],
+  [{age: 50, name: "USER 51"}, 0],
+  [[{age: 50}, {name: "USER 51"}], 3],
+  [[{age: 50}, {name: "USER 50"}], 2],
+  [{"age <": 50, "age >=": 40}, 20],
+  [{"name <": "USER 50", "name >": "USER 40"}, 10],
+  [[{age: 50}, {"name >": "USER 50", "age <": 60}], 74],
+  [undefined, 200],
+  [[], 200]
+];
+
+
+expect("load");
+var store = nStore.new('fixtures/new.db', function () {
+  fulfill("load");
+  expect("one");
+  expect("two");
+  expect("three");
+  Step(
+    function () {
+      fulfill("one");
+      var group1 = this.group();
+      var group2 = this.group();
+      for (var i = 0; i < 100; i++) {
+        store.save(null, {name: "USER " + i, age: i}, group1());
+        store.save(null, {name: "User " + i, age: i}, group2());
+      }
+    },
+    function (err, keys1, keys2) {
+      fulfill("two");
+      if (err) throw err;
+      assert.equal(store.length, 200, "There should be 200 records now");
+      var group = this.group();
+      queries.forEach(function (pair, i) {
+        store.find(pair[0], group());
+      });
+    },
+    function (err, results) {
+      fulfill("three");
+      if (err) throw err;
+      assert.equal(results.length, queries.length, "All queries should come back");
+      expect("result");
+      results.forEach(function (result, i) {
+        fulfill("result");
+        var num = Object.keys(result).length;
+        var expected = queries[i][1];
+        assert.equal(num, expected, JSON.stringify(queries[i][0]) +
+                  ": expected " + expected + " rows, but found " + num);
+      });
+    },
+    function (err) {
+      console.error(err.stack);
+    }
+  );
+
+});
