f220858822360d2dddda9d14ce518142ec6b7422
Tim Caswell <tim@creationix.com>
2010-08-18 19:17:32 -0700

    Optimize the key generation a bit.

--- test/stressTest.js
+++ test/stressTest.js
@@ -4,9 +4,12 @@ console.dir(process.memoryUsage());
 
 var counter = 0;
 const NUM = 1000000;
+var obj = {F:"B"};
 var documents = nStore.new('fixtures/new.db', function () {
   for (var i = 0; i < NUM; i++) {
-    documents.save((i % 1000) + 1, {i:i}, function () {
+    documents.save((i % 1000) + 1, obj, function () {
+    // documents.save(i, obj, function () {
+    // documents.save(null, obj, function () {
       counter++;
       if (counter === NUM) {
         documents.compactDatabase();
